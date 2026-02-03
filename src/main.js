fed769fe4b936c91d823f0af02696b09965b3449
Cosimo Spina <csxleer@gmail.com>
2026-02-03 12:35:31 +0100

    Ho ottimizzato bootstrap e cambiato qualche colore nell'index.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
-// Import our custom CSS
+// // Import our custom CSS
 import "/src/style.scss";
 
-// Import all of Bootstrap’s JS
-import * as bootstrap from "bootstrap";
+// // Import all of Bootstrap’s JS
+// import * as bootstrap from "bootstrap";
+
+// Import just what we need
+
+// import 'bootstrap/js/dist/alert';
+import "bootstrap/js/dist/button";
+// import 'bootstrap/js/dist/carousel';
+import "bootstrap/js/dist/collapse";
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/offcanvas';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
